6a3f4574cd05ce0fb60e436327ef6aefe2893c46
Myuze <Myuze@users.noreply.github.com>
2021-09-22 22:26:21 -0700

    Fixed Category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,7 +48,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const category = await Tag.update(req.body, {
+    const category = await Category.update(req.body, {
       where: {
         id: req.params.id,
       },
@@ -66,7 +66,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
-    const category = await Tag.destroy({
+    const category = await Category.destroy({
       where: { id: req.params.id }
     });
     if (!category) {
